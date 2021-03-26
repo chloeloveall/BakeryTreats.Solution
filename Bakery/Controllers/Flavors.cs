757c6c561afaef604909dfc852ca4f14f42de799
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-26 16:48:15 -0700

    Add Flavors controller with with authorization

---
+++ Bakery/Controllers/Flavors.cs
@@ -0,0 +1,28 @@
+using Bakery.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Bakery.Controllers
+{
+  [Authorize]
+  public class FlavorsController : Controller
+  {
+    private readonly BakeryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public FlavorsController(UserManager<ApplicationUser> userManager, BakeryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+    
+  }
+}

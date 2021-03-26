99dc53439881bb9e08e1013ef92e9defe63cb2fa
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-26 09:26:23 -0700

    Add POST request to register new user

--- Bakery/Controllers/AccountController.cs
+++ Bakery/Controllers/AccountController.cs
@@ -29,5 +29,20 @@ namespace Bakery.Controllers
       return View();
     }
 
+    [HttpPost]
+    public async Task<ActionResult> Register (RegisterViewModel model)
+    {
+      var user = new ApplicationUser { UserName = model.Email };
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if (result.Succeeded)
+      {
+          return RedirectToAction("Index");
+      }
+      else
+      {
+          return View();
+      }
+    }
+
   }
 }

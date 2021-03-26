0459bdf2cda950fb5432e2e630051d34646c0d85
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-26 09:16:24 -0700

    Add AccountController class, constructor, and using directives

---
+++ Bakery/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+using Bakery.Models;
+using Bakery.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Bakery.Controllers
+{
+  public class AccountController : Controller
+  {
+    private readonly BakeryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, BakeryContext db)
+    {
+      _userManager = userManager;
+      _signInManager = signInManager;
+      _db = db;
+    }
+
+  }
+}

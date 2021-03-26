3348a97b87983888b2989bcec47af4fa67fc1120
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-26 09:03:18 -0700

    Update models using directive

--- Bakery/Startup.cs
+++ Bakery/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Factory.Models;
+using Bakery.Models;
 
 namespace Bakery
 {

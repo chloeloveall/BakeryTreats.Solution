7fa3ca6ab09b97f174ca4172a78c3014b56bade6
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-27 11:34:35 -0700

    Add delete POST route to remove selected treat from flavor

--- Bakery/Controllers/Flavors.cs
+++ Bakery/Controllers/Flavors.cs
@@ -101,5 +101,14 @@ namespace Bakery.Controllers
       return RedirectToAction("Index");
     }
 
+    [HttpPost]
+    public ActionResult DeleteTreat(int joinId)
+    {
+      var joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreat.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }

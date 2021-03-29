cc00b8ba413bcf1cb9a577e8132f2a9041f110f5
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-29 12:15:57 -0700

    Add treats controller with index GET request - view ordered alphabetically

---
+++ Bakery/Controllers/TreatsController.cs
@@ -0,0 +1,28 @@
+using Bakery.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bakery.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly BakeryContext _db;
+
+    public TreatsController(BakeryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Treat> model = _db.Treats.ToList();
+      return View(_db.Treats.OrderBy(m=>m.TreatName).ToList());
+    }
+
+  }
+}
